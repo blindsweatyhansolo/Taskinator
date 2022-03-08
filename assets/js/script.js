a565355446b8bebf343c4b365c4f919adbec631f
J Semidey <mtsemidey@gmail.com>
2022-03-08 16:29:27 -0500

    updated createTaskHandler function to flesh out new tasks with values from input and selector

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to create new task
 // (li element with assigned text and class name, appended to selected ul)
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
     // prevents the default browser action of refreshing page when form is submitted
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+
+    // create div to hold task info and add to list item, give class name and html content
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // add new div to list
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
